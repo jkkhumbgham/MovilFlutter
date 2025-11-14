79e7d8326f9aebcea306c9087ca5da238701b20e
jkkhumbgham <juanalbac06@gmail.com>
2025-11-14 11:17:55 -0500

    funciona

--- trazzo_flutter/lib/main.dart
+++ trazzo_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:trazzo_flutter/Screens/subir_obra/subir_obra_screen.dart';
 import 'package:trazzo_flutter/Screens/trending/trending_screen.dart';
@@ -10,6 +11,21 @@ import 'package:trazzo_flutter/Screens/register_screen.dart';
 import 'package:trazzo_flutter/Screens/publicaciones_guardadas_screen.dart';
 import 'package:trazzo_flutter/Screens/crear_editar_comment_screen.dart';
 
+final GoRouter _router = GoRouter( routes: [
+    GoRoute(path: '/', builder: (context, state) => HomeScreen(), ),
+    GoRoute(path: '/login', builder: (context, state) => IniciarSesion(), ),
+    GoRoute(path: '/register', builder: (context, state) => RegisterScreen(), ),
+    GoRoute(path: '/main', builder: (context, state) => PrincipalScreen(), ),
+    GoRoute(path: '/saved_posts', builder: (context, state) => PublicacionesGuardadasScreen(), ),
+    GoRoute(path: '/upload_art', builder: (context, state) => const SubirObraScreen(), ),
+    GoRoute(path: '/trending', builder: (context, state) => const TrendingScreen(), ),
+    GoRoute(path: '/comment/:obraId', builder: (context, state) {
+      final obraId = state.pathParameters['obraId']!;
+      return CrearEditarCommentScreen(obraId: obraId);
+    }, ),
+]);
+
+
 void main() {
   print('Iniciando en PublicacionesGuardadasScreen');
   runApp(const MainApp());
@@ -20,22 +36,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      //home: PrincipalScreen(),
-      //home: IniciarSesion(),
-      //home: RegisterScreen(),
-
-      //home: PublicacionesGuardadasScreen(),
-      
-      home: CrearEditarCommentScreen(
-        obraId: "obra_prueba",
- ),
-      //home: const SubirObraScreen(),
-      //home: const TrendingScreen(),
-
-
-      //home: HomeScreen(),
-
-    );
+    return MaterialApp.router(routerConfig: _router);
   }
 }
